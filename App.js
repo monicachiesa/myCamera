e294611a60b9ea820f8bc1f5b47658fe45432282
Mônica Chiesa <monica.chiesa@universo.univates.br>
2021-11-15 16:53:22 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Modal, Image } from 'react-native';
+import { Camera } from 'expo-camera'
+import { FontAwesome } from "@expo/vector-icons"
+
+
+export default function App() {
+
+
+  const [type, setType] = useState(Camera.Constants.Type.back) //fala
+  //pra carregar sempre primeiro a câmera traseira
+  const [hasPermission, setHasPermission] = useState(null) //pede permissão
+  //para usar a câmera
+  const camRef = useRef(null)
+  const [capturedPhoto, setCapturedPhoto] = useState(null)
+  const [open, setOpen] = useState(false)
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync()
+      setHasPermission(status === "granted");
+    })();
+  }, [])
+
+  if (hasPermission === null) {
+    return <View />
+  }
+  if (hasPermission === false) {
+    return <Text>Acesso negado</Text>
+  }
+
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri)
+      setOpen(true)
+    }
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera
+        type={type}
+        style={styles.camera}
+        ref={camRef}
+      >
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              )
+            }} //consegue trocar a camera pela frontal ou traseira
+          >
+            <FontAwesome name="exchange" size={23} color="red">
+            </FontAwesome>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonCamera}
+            onPress={takePicture}
+
+          ><FontAwesome name="camera" size={23} color="#FFF"></FontAwesome>
+
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      {capturedPhoto && (
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={open} //abre o modal quando capturar foto
+        >
+          <View style={styles.contentModal}>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => (setOpen(false))}
+            >
+              <FontAwesome name="close" size={50} color="#FFF"></FontAwesome>
+            </TouchableOpacity>
+
+            <Image style={styles.imgPhoto} source={{ uri : capturedPhoto }} />
+          </View>
+        </Modal>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    width: "100%",
+    height: "100%"
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50
+  },
+  buttonCamera: {
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50
+  },
+  contentModal:{
+flex: 1,
+justifyContent: "center",
+alignItems: "flex-end",
+margin: 20
+  },
+  closeButton:{
+    position: "absolute",
+    top: 10,
+    left: 2,
+    margin: 10,
+  },
+  imgPhoto:{
+    width: "100%",
+    height: 400
+  }
+});
